61e982ca7ec4e7efb4967388cc1efd8de8e9f73b
PedroViniciusz <pedrovdjesus@hotmail.com>
2022-11-21 14:49:00 -0300

    Configurações iniciais do Spring Boot

---
+++ src/test/java/com/example/unicar/UnicarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
